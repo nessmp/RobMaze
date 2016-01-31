845bc6d17eedd79815fc7d3c2f5fcd22afb9d7e7
pracknes <nestormp27@gmail.com>
2016-01-30 21:39:31 -0600

    ultrasonico

---
+++ Ultrasonicos/ultrasonico_simple/ultrasonico_simple.ino
@@ -0,0 +1,29 @@
+#include <NewPing.h>
+#define MAX_DISTANCE 200
+//ultrasonicos de enfrente
+byte bTriggerE1 = 8;
+byte bEchoE1 = 9;
+
+NewPing sonarE1(bTriggerE1, bEchoE1, MAX_DISTANCE);  //llamar a la funcion para saber la distancia con sonarE1.ping_cm();
+
+byte bTriggerE2 = 10;
+byte bEchoE2 = 11;
+
+NewPing sonarE2(bTriggerE2, bEchoE2, MAX_DISTANCE);  //llamar a la funcion para saber la distancia con sonarE1.ping_cm();
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.print("Ultrasonico Derecha: ");
+  Serial.println(sonarE1.ping_cm());
+  delay(500);
+
+  Serial.print("Ultrasonico Izquierda: ");
+  Serial.println(sonarE2.ping_cm());
+  delay(500);
+}
