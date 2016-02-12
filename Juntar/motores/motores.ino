ff20955b6e6ee6e175f7c4d212092913883a0e2c
alexhor22 <alexhor22@gmail.com>
2016-02-12 15:14:13 -0600

    juntar

--- Juntar/motores/motores.ino
+++ Juntar/motores/motores.ino
@@ -1,3 +1,10 @@
+#include <Encoder.h>
+
+Encoder myEnc(2, 3); //atras derecha
+Encoder myEnc2(18, 19);
+Encoder myEnc3(20, 21);
+
+long oldPosition  = -999;
 /////////////////////////
 //pines de los motores///
 /////////////////////////
@@ -12,18 +19,18 @@ byte motIzqE1 = 8;
 byte motIzqE2 = 9;
 
 byte motIzqA1 = 10;
-byte motIzqA2 =11;
+byte motIzqA2 = 11;
 
 void setup() {
   Serial.begin(9600);
-  
+
   ////////////////////////
   //Setup de los motores//
   ////////////////////////
-  
+
   pinMode(motDerE1, OUTPUT);
   pinMode(motDerE2, OUTPUT);
-  
+
   pinMode(motDerA1, OUTPUT);
   pinMode(motDerA2, OUTPUT);
 
@@ -88,13 +95,29 @@ void Atras()
 }
 
 //funcion para moverse hacia adelante
-void Derecha()
+void DerechaM()
 {
-  analogWrite(motDerE1, 255);
-  analogWrite(motDerE2, 0);
+  analogWrite(motDerE1, 0);
+  analogWrite(motDerE2, 255);
 
   analogWrite(motDerA1, 255);
   analogWrite(motDerA2, 0);
+
+  analogWrite(motIzqE1, 255);
+  analogWrite(motIzqE2, 0);
+
+  analogWrite(motIzqA1, 0);
+  analogWrite(motIzqA2, 255);
+
+}
+
+void Derecha()
+{
+  analogWrite(motDerE1, 0);
+  analogWrite(motDerE2, 255);
+
+  analogWrite(motDerA1, 0);
+  analogWrite(motDerA2, 255);
 
   analogWrite(motIzqE1, 255);
   analogWrite(motIzqE2, 0);
@@ -105,13 +128,28 @@ void Derecha()
 }
 
 //funcion para moverse hacia izquierda
-void Izquierda()
+void IzquierdaM()
 {
-  analogWrite(motDerE1, 0);
-  analogWrite(motDerE2, 255);
+  analogWrite(motDerE1, 255);
+  analogWrite(motDerE2, 0);
 
   analogWrite(motDerA1, 0);
   analogWrite(motDerA2, 255);
+
+  analogWrite(motIzqE1, 0);
+  analogWrite(motIzqE2, 255);
+
+  analogWrite(motIzqA1, 255);
+  analogWrite(motIzqA2, 0);
+}
+
+void Izquierda()
+{
+  analogWrite(motDerE1, 255);
+  analogWrite(motDerE2, 0);
+
+  analogWrite(motDerA1, 255);
+  analogWrite(motDerA2, 0);
 
   analogWrite(motIzqE1, 0);
   analogWrite(motIzqE2, 255);
@@ -120,7 +158,54 @@ void Izquierda()
   analogWrite(motIzqA2, 255);
 }
 
+
+int Encoder1()
+{
+
+  long newPosition = myEnc.read();
+
+
+  if (newPosition != oldPosition) {
+    oldPosition = newPosition;
+    Serial.println(newPosition);
+
+  }
+  return newPosition;
+}
+
+void GiroDer90()
+{
+    int const90 = 4550;
+  delay(1000);
+  myEnc.write(0);
+ while (Encoder1() > const90*-1)
+  {
+    Derecha();
+    Encoder1();
+  }
+   Detenerse();
+}
+
+void GiroIsq90()
+{
+  int const90 = 4550;
+  delay(1000);
+  myEnc.write(0);
+
+ 
+ 
+  while (Encoder1() < const90)
+  {
+    Izquierda();
+    Encoder1();
+  }
+   Detenerse();
+}
+
 void loop() {
   // put your main code here, to run repeatedly:
- Adelante();
+DerechaM();
+delay(3000);
+IzquierdaM();
+delay(3000);
 }
--- Sensores y cosas simples/encoders/con_libreria/con_libreria.ino
+++ Sensores y cosas simples/encoders/con_libreria/con_libreria.ino
@@ -10,7 +10,8 @@
 //   Best Performance: both pins have interrupt capability
 //   Good Performance: only the first pin has interrupt capability
 //   Low Performance:  neither pin has interrupt capability
-Encoder myEnc(2, 4);
+Encoder myEnc1(2, 3);
+
 //   avoid using pins with LEDs attached
 
 void setup() {
@@ -20,10 +21,14 @@ void setup() {
 
 long oldPosition  = -999;
 
+
 void loop() {
   long newPosition = myEnc.read();
+
+
   if (newPosition != oldPosition) {
     oldPosition = newPosition;
-    Serial.println(newPosition);
+    
+    Serial.println(newPosition); 
   }
 }
