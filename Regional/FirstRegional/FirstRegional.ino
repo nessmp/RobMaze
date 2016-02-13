19c31ca8472efbe775f0ce1c78a8f85abfaf9502
pracknes <nestormp27@gmail.com>
2016-02-13 05:24:25 -0600

    avances

--- Regional/FirstRegional/FirstRegional.ino
+++ Regional/FirstRegional/FirstRegional.ino
@@ -7,6 +7,7 @@
 #define MAX_DISTANCE 200 //distancia max detectada por los ultrasonicos
 #include <SharpIR.h> //sharps
 #define model 1080 //modelo del sharp GP2Y0A21Y
+#include <i2cmaster.h>
 
 /////////
 //pines//
@@ -168,6 +169,10 @@ void setup() {
   //Sharps
   pinMode (Enf, INPUT);
   pinMode (Der, INPUT);
+
+  //Calor
+  i2c_init(); //Initialise the i2c bus
+  PORTC = (1 << PORTC4) | (1 << PORTC5);//enable pullups
 }
 
 /////////////
@@ -175,8 +180,9 @@ void setup() {
 /////////////
 
 //función para leer datos del sensor de color
-void color()  
+bool color()  
 {    
+  bool Negro = false;
   digitalWrite(s2, LOW);  
   digitalWrite(s3, LOW);  
   //count OUT, pRed, RED  
@@ -194,6 +200,21 @@ void color()
   Serial.println(green);
   Serial.println("blue: ");
   Serial.println(blue);
+
+  if(red && green && blue)
+  {
+    Negro = true;
+  }
+
+  return Negro;
+}
+
+//Acciones al encontrar zona negra
+void Negro()
+{
+  Atras30();
+  GiroIzq90();
+  Adelante30();
 }
 
 ////////////////////
@@ -349,24 +370,133 @@ int SharpDer()
   return SharpDer.distance()
 }
 
+//regresa el calor detectado por el sensor de enfrente, REVISAR EL ADDRESS
 int CalorEnf()
 {
-  return CalorEnf.object();
+    int Calor;
+    int dev = 0x1C<<1;
+    int data_low = 0;
+    int data_high = 0;
+    int pec = 0;
+    
+    i2c_start_wait(dev+I2C_WRITE);
+    i2c_write(0x07);
+    
+    // read
+    i2c_rep_start(dev+I2C_READ);
+    data_low = i2c_readAck(); //Read 1 byte and then send ack
+    data_high = i2c_readAck(); //Read 1 byte and then send ack
+    pec = i2c_readNak();
+    i2c_stop();
+    
+    //This converts high and low bytes together and processes temperature, MSB is a error bit and is ignored for temps
+    double tempFactor = 0.02; // 0.02 degrees per LSB (measurement resolution of the MLX90614)
+    double tempData = 0x0000; // zero out the data
+    int frac; // data past the decimal point
+    
+    // This masks off the error bit of the high byte, then moves it left 8 bits and adds the low byte.
+    tempData = (double)(((data_high & 0x007F) << 8) + data_low);
+    tempData = (tempData * tempFactor)-0.01;
+    
+    float celcius = tempData - 273.15;
+    Calor = celcius;
+    return Calor;
 }
 
 int CalorDer()
 {
-  return CalorDer.object();
+    int Calor;
+    int dev = 0x2C<<1;
+    int data_low = 0;
+    int data_high = 0;
+    int pec = 0;
+    
+    i2c_start_wait(dev+I2C_WRITE);
+    i2c_write(0x07);
+    
+    // read
+    i2c_rep_start(dev+I2C_READ);
+    data_low = i2c_readAck(); //Read 1 byte and then send ack
+    data_high = i2c_readAck(); //Read 1 byte and then send ack
+    pec = i2c_readNak();
+    i2c_stop();
+    
+    //This converts high and low bytes together and processes temperature, MSB is a error bit and is ignored for temps
+    double tempFactor = 0.02; // 0.02 degrees per LSB (measurement resolution of the MLX90614)
+    double tempData = 0x0000; // zero out the data
+    int frac; // data past the decimal point
+    
+    // This masks off the error bit of the high byte, then moves it left 8 bits and adds the low byte.
+    tempData = (double)(((data_high & 0x007F) << 8) + data_low);
+    tempData = (tempData * tempFactor)-0.01;
+    
+    float celcius = tempData - 273.15;
+    Calor = celcius;
+    return Calor;
 }
 
 int CalorAtr()
 {
-  return CalorAtr.object();
+    int Calor;
+    int dev = 0x3C<<1;
+    int data_low = 0;
+    int data_high = 0;
+    int pec = 0;
+    
+    i2c_start_wait(dev+I2C_WRITE);
+    i2c_write(0x07);
+    
+    // read
+    i2c_rep_start(dev+I2C_READ);
+    data_low = i2c_readAck(); //Read 1 byte and then send ack
+    data_high = i2c_readAck(); //Read 1 byte and then send ack
+    pec = i2c_readNak();
+    i2c_stop();
+    
+    //This converts high and low bytes together and processes temperature, MSB is a error bit and is ignored for temps
+    double tempFactor = 0.02; // 0.02 degrees per LSB (measurement resolution of the MLX90614)
+    double tempData = 0x0000; // zero out the data
+    int frac; // data past the decimal point
+    
+    // This masks off the error bit of the high byte, then moves it left 8 bits and adds the low byte.
+    tempData = (double)(((data_high & 0x007F) << 8) + data_low);
+    tempData = (tempData * tempFactor)-0.01;
+    
+    float celcius = tempData - 273.15;
+    Calor = celcius;
+    return Calor;
 }
 
 int CalorIzq()
 {
-  return CalorIzq.object();
+    int Calor;
+    int dev = 0x4C<<1;
+    int data_low = 0;
+    int data_high = 0;
+    int pec = 0;
+    
+    i2c_start_wait(dev+I2C_WRITE);
+    i2c_write(0x07);
+    
+    // read
+    i2c_rep_start(dev+I2C_READ);
+    data_low = i2c_readAck(); //Read 1 byte and then send ack
+    data_high = i2c_readAck(); //Read 1 byte and then send ack
+    pec = i2c_readNak();
+    i2c_stop();
+    
+    //This converts high and low bytes together and processes temperature, MSB is a error bit and is ignored for temps
+    double tempFactor = 0.02; // 0.02 degrees per LSB (measurement resolution of the MLX90614)
+    double tempData = 0x0000; // zero out the data
+    int frac; // data past the decimal point
+    
+    // This masks off the error bit of the high byte, then moves it left 8 bits and adds the low byte.
+    tempData = (double)(((data_high & 0x007F) << 8) + data_low);
+    tempData = (tempData * tempFactor)-0.01;
+    
+    float celcius = tempData - 273.15;
+    Calor = celcius;
+    return Calor;
 }
 
 void Blink()
@@ -400,8 +530,16 @@ void EncIzqA()
   return EncIzqA.read();
 }
 
+void AgujeroNegro()
+{
+  if(color() == true)
+  {
+    Negro();
+  }
+}
+
+
+
 void loop() {
   // put your main code here, to run repeatedly:
-  Serial.println(CalorIzq);
-  delay(20);
 }
